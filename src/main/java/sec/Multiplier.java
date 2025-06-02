52f26745f9f32d0f424b8c9538c0de6b76348d1c
notYourOcean <wjswjdgus333@gmail.com>
2025-06-02 22:38:01 +0900

    multiplier modify

--- src/main/java/sec/Multiplier.java
+++ src/main/java/sec/Multiplier.java
@@ -1,11 +1,24 @@
 package sec;
 
-public class Multiplier implements IMultiplier{
+public class Multiplier implements IMultiplier {
+    private IAdder adder;
+    private IFlipper flipper;
+
     public Multiplier(IAdder adder, IFlipper flipper) {
+        this.adder = adder;
+        this.flipper = flipper;
     }
 
-    @Override
-    public int multiply(int number1, int number2) {
-        return number1 * number2;
+    public int multiply(int a, int b) {
+        if (b < 0) {
+            a = flipper.flip(a);
+            b = flipper.flip(b);
+        }
+
+        int result = 0;
+        for (int i = 0; i < b; i++) {
+            result = adder.add(result, a);
+        }
+        return result;
     }
 }
